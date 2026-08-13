9085eb7e737befa55a1d90852d1a2ec5ccbcac65
Othavio Nogueira <othavionogueira2003@gmail.com>
2026-08-13 18:07:15 -0300

    feat: add methods get, delete and login

--- src/main/java/com/othavio/usuario/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/othavio/usuario/infrastructure/repository/UsuarioRepository.java
@@ -12,4 +12,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     boolean existsByEmail(String email);
 
     Optional<Usuario> findByEmail(String email);
+
+    void deleteByEmail(String email);
 }
